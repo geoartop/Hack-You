e0b49c8e1bc5d651b0dd2ecd8d7ffe5314e4e389
Theodoros Malikourtis <93033176+theomalikourtis@users.noreply.github.com>
2021-10-23 13:37:39 +0300

    Update Mallo.java

--- Spinollis/src/Mallo.java
+++ Spinollis/src/Mallo.java
@@ -1,6 +1,7 @@
 public class Mallo {
     public static void main(String[] args) {
         System.out.println("Patao next paidia?");
+        System.out.println("test test")
         int x=4012;
     }
 }
