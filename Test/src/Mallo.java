011d348c63bfd9850f867dedb68a3f3319430ebd
Panagiotis Spanakis <93033399+panos-span@users.noreply.github.com>
2021-11-01 20:45:09 +0200

    Update Mallo.java

--- Test/src/Mallo.java
+++ Test/src/Mallo.java
@@ -1,13 +1,11 @@
 public class Mallo {
     public static void main(String[] args) {
-        System.out.println("Patao next paidia?");
         System.out.println("test test");
         int x = 4012;
         int rigas = 45;
         if (rigas == 45)
             System.out.println("PHramee protathlima");
         System.out.println("Saki pou eisai?");
-        //SPINOLLL
         System.out.println("SPYROS");
         // test //
         System.out.println(x);
