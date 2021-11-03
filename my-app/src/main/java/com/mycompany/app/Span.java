ff72e75170a8312a2e637b6b76ddd769e601aa6a
Panagiotis Spanakis <t8200158@aueb.gr>
2021-11-03 22:35:01 +0200

    Maven

---
+++ my-app/src/main/java/com/mycompany/app/Span.java
@@ -0,0 +1,8 @@
+package com.mycompany.app;
+
+public class Span {
+    public static void main(String[] args) {
+        System.out.println("I have no idea what the fuck im doing!!");
+        throw new IndexOutOfBoundsException("Send Help");
+    }
+}
