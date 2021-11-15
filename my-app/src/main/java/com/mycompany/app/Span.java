81d85e587b39fccab60e77bfaa580bc379b6fb47
Panagiotis Spanakis <t8200158@aueb.gr>
2021-11-15 16:44:35 +0200

    Kali phimosi

--- my-app/src/main/java/com/mycompany/app/Span.java
+++ my-app/src/main/java/com/mycompany/app/Span.java
@@ -3,6 +3,7 @@ package com.mycompany.app;
 public class Span {
     public static void main(String[] args) {
         System.out.println("I have no idea what the fuck im doing!!");
+        int x=9;
         throw new IndexOutOfBoundsException("Send Help");
     }
 }
