d681fe6df415248b3d18e75205820d339c3d7ee8
Panagiotis Spanakis <t8200158@aueb.gr>
2021-11-17 19:27:09 +0200

    Read files

--- Maven/src/main/java/cli/ReadText.java
+++ Maven/src/main/java/cli/ReadText.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 public class ReadText {
 
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner sc = new Scanner(new File("testfile.txt"));
+        Scanner sc = new Scanner(new File("src/main/resources/testfile.txt"));
         List<String> lines = new ArrayList<String>();
         while (sc.hasNextLine()) {
             lines.add(sc.nextLine());
