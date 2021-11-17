9680d993b98de3d795171f6c9be1e5daaa265145
Panagiotis Spanakis <93033399+panos-span@users.noreply.github.com>
2021-11-17 23:53:28 +0200

    Delete my-app directory

--- my-app/src/main/java/com/mycompany/app/Span.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.app;
-
-public class Span {
-    public static void main(String[] args) {
-        System.out.println("I have no idea what the fuck im doing!!");
-        int x=9;
-        throw new IndexOutOfBoundsException("Send Help");
-    }
-}
