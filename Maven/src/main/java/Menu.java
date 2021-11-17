6f7800e2dfcd156b14a04a4e5215402ea9f492a4
Panagiotis Spanakis <t8200158@aueb.gr>
2021-11-17 13:45:12 +0200

    Add Classes

--- Maven/src/main/java/Menu.java
+++ Maven/src/main/java/Menu.java
@@ -1,33 +1,5 @@
 import java.awt.event.*;
 import javax.swing.*;
 public class Menu {
-    static class AlertAction implements ActionListener {
-        private JFrame parent;
 
-        AlertAction(JFrame parent) {
-            this.parent = parent;
-        }
-
-        @Override public void actionPerformed(ActionEvent e) {
-            JOptionPane.showMessageDialog(parent, "information", "Button Pressed!!", JOptionPane.INFORMATION_MESSAGE);
-        }
-    }
-    public static void main(String[] args) {
-        JFrame f=new JFrame("Menu");
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        final JTextField tf=new JTextField();
-        tf.setBounds(50,50, 150,20);
-        JButton b1=new JButton("Start Game");
-        b1.setBounds(150,150,150,30);
-        b1.addActionListener(new AlertAction(f));
-        f.add(b1);f.add(tf);
-        JButton b2=new JButton("Login");
-
-        f.add(b2);
-        b2.setBounds(150,250,100,30);
-        f.setSize(400,400);
-        f.setLayout(null);
-        f.setVisible(true);
-
-    }
 }
--- Maven/src/main/java/Username.java
+++ Maven/src/main/java/Username.java
@@ -4,22 +4,22 @@ import javax.swing.*;
 import java.awt.event.*;
 
 public class Username {
-    private JFrame f = new JFrame("Login");
-    private JButton bok = new JButton("OK");
 
     public Username() {
-
+        JFrame f = new JFrame("Login");
+        JButton bok = new JButton("OK");
+        bok.setBounds(150,200,100,30);
+        f.add(bok);
         f.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-        f.getContentPane().add(bok);
-
         bok.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ae) {
                 f.dispose();
                 new SecondFrame();
             }
         });
-        f.setSize(100, 100);
+        f.setSize(600,600);
         f.setVisible(true);
+        f.setLayout(null);
     }
 
     public static void main(String[] args) {
