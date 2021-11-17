f22dd1470a3d460e122998c1d4010a13b4678b7f
Theodoros Malikourtis <t8200097@aueb.gr>
2021-11-17 17:51:36 +0200

    Read text from file and store in Arraylist

---
+++ Maven/src/main/java/cli/ReadText.java
@@ -0,0 +1,21 @@
+package cli;
+
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+public class ReadText {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("testfile.txt"));
+        List<String> lines = new ArrayList<String>();
+        while (sc.hasNextLine()) {
+            lines.add(sc.nextLine());
+        }
+
+        System.out.println(lines);
+
+    }
+}
