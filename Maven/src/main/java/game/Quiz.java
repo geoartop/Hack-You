066fce44e000e65a353ec38df020af58aa6a9094
Panagiotis Spanakis <t8200158@aueb.gr>
2021-11-26 13:06:33 +0200

    Implement Quiz to LabyrinthFrame

--- Maven/src/main/java/game/Quiz.java
+++ Maven/src/main/java/game/Quiz.java
@@ -1,167 +1,115 @@
 package game;
-import java.awt.event.*;
-import java.awt.*;
-import java.util.Random;
+
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
-
-
+//Waiting for explanation by mallikok
 public class Quiz implements ActionListener {
     ArrayList<String> questions = new ArrayList<String>();
     ArrayList<String> options = new ArrayList<String>();
     ArrayList<Character> answers = new ArrayList<Character>();
     char answer;
-    Random random = new Random();
+    private final Random random = new Random();
     int index;
 
-    //------------------------------//----------------------------------------//
-    protected static void setQuestionsDifficulty(){
-        switch (Levels.difficulty) {
-            case "easy":
-                // JOptionPane.showMessageDialog(null,"1","ferwu",JOptionPane.INFORMATION_MESSAGE);
-                //file=EasyQuestions.txt
-                break;
-            case "medium":
-                //file=MediumQuestions.txt
-                break;
-            default:
-                //file=HardQuestions.txt
-                break;
-        }
-    }
-     //------------------------------//----------------------------------------//
-
     JFrame frame = new JFrame();
-    JTextField textField = new JTextField();
+    //JTextField textField = new JTextField();
     JTextArea textArea = new JTextArea();
-    JButton buttonA = new JButton();
-    JButton buttonB = new JButton();
-    JButton buttonC = new JButton();
-    JButton buttonD = new JButton();
-    JLabel labelA = new JLabel();
-    JLabel labelB = new JLabel();
-    JLabel labelC = new JLabel();
-    JLabel labelD = new JLabel();
+
+    JButton[] buttons = new JButton[4];
+    char[] symbols = {'A', 'B', 'C', 'D'};
+
+    JLabel label = new JLabel();
+    JLabel[] labels = new JLabel[4];
 
     public Quiz() throws FileNotFoundException {
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         frame.setSize(700, 550);
         frame.setLayout(null);
         frame.setResizable(false);
+        frame.setTitle("Question"); //setTitle of frame
+        frame.setIconImage(Main.icon.getImage());
+        //Για να εμφανίζεται στο κέντρο της οθόνης του χρήστη
+        frame.setLocationRelativeTo(null);
 
-        textField.setBounds(0, 0, 700, 50);
-        textField.setFont(new Font("Calibri", Font.BOLD, 25));
-        textField.setBorder(BorderFactory.createBevelBorder(1));
-        textField.setHorizontalAlignment(JTextField.CENTER);
-        textField.setEditable(false);
-
-
-        textArea.setBounds(0, 50, 700, 50);
+        textArea.setBounds(0, 0, 700, 100);
         textArea.setLineWrap(true);
         textArea.setWrapStyleWord(true);
-        textArea.setBackground(new Color(25, 25, 25));
+        //textArea.setBackground(new Color(25, 25, 25));
+        textArea.setOpaque(true);
         textArea.setForeground(new Color(25, 255, 0));
-        textArea.setFont(new Font("Calibri", Font.BOLD, 25));
+        textArea.setFont(new Font("Calibri", Font.BOLD, 20));
         textArea.setBorder(BorderFactory.createBevelBorder(1));
         textArea.setEditable(false);
 
-        buttonA.setBounds(0, 100, 100, 100);
-        buttonA.setFont(new Font("Calibri", Font.BOLD, 35));
-        buttonA.setFocusable(false);
-        buttonA.addActionListener(this);
-        buttonA.setText("A");
-
-        buttonB.setBounds(0, 200, 100, 100);
-        buttonB.setFont(new Font("Calibri", Font.BOLD, 35));
-        buttonB.setFocusable(false);
-        buttonB.addActionListener(this);
-        buttonB.setText("B");
-
-        buttonC.setBounds(0, 300, 100, 100);
-        buttonC.setFont(new Font("Calibri", Font.BOLD, 35));
-        buttonC.setFocusable(false);
-        buttonC.addActionListener(this);
-        buttonC.setText("C");
-
-        buttonD.setBounds(0, 400, 100, 100);
-        buttonD.setFont(new Font("Calibri", Font.BOLD, 35));
-        buttonD.setFocusable(false);
-        buttonD.addActionListener(this);
-        buttonD.setText("D");
-
-        labelA.setBounds(125, 100, 500, 100);
-        labelA.setBackground(new Color(50, 50, 50));
-        labelA.setForeground(new Color(0, 72, 255));
-        labelA.setFont(new Font("Calibri", Font.PLAIN, 20));
-
-
-        labelB.setBounds(125, 200, 500, 100);
-        labelB.setBackground(new Color(50, 50, 50));
-        labelB.setForeground(new Color(0, 72, 255));
-        labelB.setFont(new Font("Calibri", Font.PLAIN, 20));
-
-        labelC.setBounds(125, 300, 500, 100);
-        labelC.setBackground(new Color(50, 50, 50));
-        labelC.setForeground(new Color(0, 72, 255));
-        labelC.setFont(new Font("Calibri", Font.PLAIN, 20));
-
-        labelD.setBounds(125, 400, 500, 100);
-        labelD.setBackground(new Color(50, 50, 50));
-        labelD.setForeground(new Color(0, 72, 255));
-        labelD.setFont(new Font("Calibri", Font.PLAIN, 20));
-
-        frame.add(labelA);
-        frame.add(labelB);
-        frame.add(labelC);
-        frame.add(labelD);
-        frame.add(buttonA);
-        frame.add(buttonB);
-        frame.add(buttonC);
-        frame.add(buttonD);
+        setLabels();
+        setButtons();
+        for (JLabel label : labels)
+            frame.add(label);
+        for (JButton button : buttons)
+            frame.add(button);
         frame.add(textArea);
-        frame.add(textField);
+        //frame.add(textField);
         frame.setVisible(true);
-
-
         readQuestions();
         index = random.nextInt(questions.size());
-
         displayQuestion();
 
+        Image img = Main.background.getImage();
+        Image temp = img.getScaledInstance(685, 550, Image.SCALE_SMOOTH);
+        ImageIcon back = new ImageIcon(temp);
+        label.setIcon(back);
+        label.setBounds(0, 0, 700, 550);
+        //Για να εμφανίζεται στο κέντρο της οθόνης του χρήστη
+        frame.add(label);
     }
 
     public void displayQuestion() {
-        textField.setText("Game");
+        //textField.setText("Game");
         textArea.setText(questions.get(index));
-        labelA.setText(options.get(4 * index));
-        labelB.setText(options.get(4 * index + 1));
-        labelC.setText(options.get(4 * index + 2));
-        labelD.setText(options.get(4 * index + 3));
+        for (int i = 0; i < labels.length; i++)
+            labels[i].setText(options.get(4 * index + i));
 
     }
 
+    public void setButtons() {
+        for (int i = 0; i < buttons.length; i++) {
+            buttons[i] = new JButton();
+            buttons[i].setBounds(0, (i + 1) * 100, 100, 100);
+            buttons[i].setFont(new Font("Calibri", Font.BOLD, 35));
+            buttons[i].setFocusable(false);
+            buttons[i].addActionListener(this);
+            buttons[i].setText(String.valueOf(symbols[i]));
+        }
+    }
+
+    public void setLabels() {
+        for (int i = 0; i < labels.length; i++) {
+            labels[i] = new JLabel();
+            labels[i].setBounds(125, (i + 1) * 100, 500, 100);
+            labels[i].setBackground(new Color(50, 50, 50));
+            labels[i].setForeground(new Color(0, 72, 255));
+            labels[i].setFont(new Font("Calibri", Font.PLAIN, 20));
+        }
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == buttonA) {
-            answer = 'A';
-        } else if (e.getSource() == buttonB) {
-            answer = 'B';
-        } else if (e.getSource() == buttonC) {
-            answer = 'C';
-
-        } else if (e.getSource() == buttonD) {
-            answer = 'D';
+        for (int i = 0; i < buttons.length; i++) {
+            if (e.getSource() == buttons[i]) {
+                answer = symbols[i];
+                break;
+            }
         }
         checkAnswer();
         frame.dispose();
-
-
     }
 
     public void correctAnswer() {
@@ -175,28 +123,31 @@ public class Quiz implements ActionListener {
     }
 
     public void checkAnswer() {
+        int time;
         if (answer == answers.get(index)) {
+            time=LabyrinthFrame.for_correct;
             correctAnswer();
-
-        } else wrongAnswer();
+        } else{
+            time=LabyrinthFrame.for_wrong;
+            wrongAnswer();
+        }
+        LabyrinthFrame.updateBar(time);
+        frame.dispose();
 
     }
 
     public void readQuestions() throws FileNotFoundException {
-        Scanner q = new Scanner(new File("src/Easy Questions.txt"));
+        Scanner q = new Scanner(new File(String.format("src/main/resources/%s Questions.txt",Levels.difficulty)));
         while (q.hasNextLine()) {
             questions.add(q.nextLine());
         }
-        Scanner o = new Scanner(new File("src/Easy Options.txt"));
+        Scanner o = new Scanner(new File(String.format("src/main/resources/%s Options.txt",Levels.difficulty)));
         while (o.hasNextLine()) {
             options.add(o.nextLine());
         }
-        Scanner a = new Scanner(new File("src/Easy Answers.txt"));
+        Scanner a = new Scanner(new File(String.format("src/main/resources/%s Answers.txt",Levels.difficulty)));
         while (a.hasNext()) {
             answers.add(a.next().charAt(0));
         }
     }
 }
-
-
-
