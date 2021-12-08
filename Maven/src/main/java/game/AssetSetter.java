1f77642b5eb0e43ecd20eaaf87f8e8059000c098
Emmanouil Dellatolas <t8200037@aueb.gr>
2021-12-08 21:27:04 +0200

    Customize object position for larger map

--- Maven/src/main/java/game/AssetSetter.java
+++ Maven/src/main/java/game/AssetSetter.java
@@ -8,9 +8,10 @@ public class AssetSetter {
     }
 
     public void setObject() {
+
         gp.obj[0] = new OBJ_Question();
         gp.obj[0].worldX = 1 * gp.tileSize; // tile row - 1
-        gp.obj[0].worldY = 4 * gp.tileSize; // tile col - 1
+        gp.obj[0].worldY = 6 * gp.tileSize; // tile col - 1
 
         gp.obj[1] = new OBJ_Question();
         gp.obj[1].worldX = 6 * gp.tileSize;
@@ -35,5 +36,38 @@ public class AssetSetter {
         gp.obj[6] = new OBJ_Exit();
         gp.obj[6].worldX = 15 * gp.tileSize;
         gp.obj[6].worldY = 1 * gp.tileSize;
+
+
+        /*
+        gp.obj[0] = new OBJ_Question();
+        gp.obj[0].worldX = 1 * gp.tileSize; // tile row - 1
+        gp.obj[0].worldY = 9 * gp.tileSize; // tile col - 1
+
+        gp.obj[1] = new OBJ_Question();
+        gp.obj[1].worldX = 6 * gp.tileSize;
+        gp.obj[1].worldY = 4 * gp.tileSize;
+
+        gp.obj[2] = new OBJ_Question();
+        gp.obj[2].worldX = 5 * gp.tileSize;
+        gp.obj[2].worldY = 13 * gp.tileSize;
+
+        gp.obj[3] = new OBJ_Question();
+        gp.obj[3].worldX = 11 * gp.tileSize;
+        gp.obj[3].worldY = 12 * gp.tileSize;
+
+        gp.obj[4] = new OBJ_Question();
+        gp.obj[4].worldX = 12 * gp.tileSize;
+        gp.obj[4].worldY = 5 * gp.tileSize;
+
+        gp.obj[5] = new OBJ_Question();
+        gp.obj[5].worldX = 8 * gp.tileSize;
+        gp.obj[5].worldY = 9 * gp.tileSize;
+
+        gp.obj[6] = new OBJ_Exit();
+        gp.obj[6].worldX = 15 * gp.tileSize;
+        gp.obj[6].worldY = 4 * gp.tileSize;
+
+         */
+
     }
 }
